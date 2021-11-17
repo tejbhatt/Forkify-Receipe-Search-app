6687a2c36937eba5bccd6eb6b10b27f7d1b44a66
tejbhatt <59688305+tejbhatt@users.noreply.github.com>
2021-11-17 19:52:51 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
-export const baseURL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
-export const PAGINATION_PAGE_COUNT = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '<YOUR_KEY>';
+export const MODAL_CLOSE_SEC = 2.5;
